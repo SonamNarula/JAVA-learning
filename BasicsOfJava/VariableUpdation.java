b735fb66414069e5a0ab95d7d9fb9083a8c255c2
SonamNarula <sonamnarula2108@gmail.com>
2025-07-28 05:45:27 +0530

    Variable Updation

---
+++ BasicsOfJava/VariableUpdation.java
@@ -0,0 +1,17 @@
+package BasicsOfJava;
+
+public class VariableUpdation {
+    public static void main(String[] args) {
+        int x = 5;
+        System.out.println(x);
+
+        x = 7;
+        System.out.println(x); //7
+
+        x = x + 6;
+        System.out.println(x); //13
+
+        x = x - 20;
+        System.out.println(x); // -7
+    }
+}
