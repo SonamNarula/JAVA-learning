72ed1ce5bcd34c7ed8c7f9199dad394a3a700331
SonamNarula <sonamnarula2108@gmail.com>
2025-07-28 06:20:34 +0530

    Double DataType

---
+++ BasicsOfJava/DoubleDataType.java
@@ -0,0 +1,9 @@
+package BasicsOfJava;
+
+public class DoubleDataType {
+    public static void main(String[] args) {
+        double x = 5;
+        double y = 2;
+        System.out.println(x / y);
+    }
+}
