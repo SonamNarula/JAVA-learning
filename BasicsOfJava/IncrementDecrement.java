fd02bd4f8024b573879f8b3521d1c9efce575c18
SonamNarula <sonamnarula2108@gmail.com>
2025-07-28 06:10:01 +0530

    Increment Decrement

---
+++ BasicsOfJava/IncrementDecrement.java
@@ -0,0 +1,23 @@
+package BasicsOfJava;
+
+public class IncrementDecrement {
+    public static void main(String[] args) {
+        int x = 5;
+        x++;
+        System.out.println(x);
+//        6
+
+        x--;
+        System.out.println(x);
+//      5
+
+        ++x;
+        System.out.println(x);
+//      6
+
+        --x;
+        System.out.println(x);
+//        5
+    }
+
+}
