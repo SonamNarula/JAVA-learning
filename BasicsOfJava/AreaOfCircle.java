d01285561861c7c70919ee6de7dc8a09db0bcf1b
SonamNarula <sonamnarula2108@gmail.com>
2025-07-28 06:29:16 +0530

    Area Of Circle

---
+++ BasicsOfJava/AreaOfCircle.java
@@ -0,0 +1,12 @@
+package BasicsOfJava;
+
+public class AreaOfCircle {
+    public static void main(String[] args) {
+        double radius = 5;
+        double pi = 3.14159;
+        double area = radius * radius * pi;
+
+        System.out.println("Area of circle is " + area);
+
+    }
+}
