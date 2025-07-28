34e5fbbcd6073b9e563245ae784ed23ca94e1b66
SonamNarula <sonamnarula2108@gmail.com>
2025-07-28 05:48:48 +0530

    Arithmetic Operators

---
+++ BasicsOfJava/ArithmeticOperators.java
@@ -0,0 +1,13 @@
+package BasicsOfJava;
+
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 2;
+
+        System.out.println(x + y); // 7
+        System.out.println(x - y); // 3
+        System.out.println(x * y); // 10
+        System.out.println(x / y); // 2
+    }
+}
