fc4b532e0d4a2c744d458c5ca2a79e9582b5fd8f
SonamNarula <sonamnarula2108@gmail.com>
2025-07-28 06:26:34 +0530

    Percentage of 5 subjects

---
+++ BasicsOfJava/Percentage_of_5_subjects.java
@@ -0,0 +1,15 @@
+package BasicsOfJava;
+
+public class Percentage_of_5_subjects {
+    public static void main(String[] args) {
+        double x1 = 90;
+        double x2 = 91;
+        double x3 = 92;
+        double x4 = 93;
+        double x5 = 94;
+
+        double percentage = (x1 + x2 + x3 + x4 + x5) / 5;
+        System.out.println(percentage);
+    }
+
+}
