7d1e3a2251abcd7d3d57994f7b0e9419bcddd9d0
SonamNarula <sonamnarula2108@gmail.com>
2025-07-28 05:39:44 +0530

    Printing Numbers

---
+++ BasicsOfJava/variablesint.java
@@ -0,0 +1,13 @@
+package BasicsOfJava;
+
+public class variablesint {
+    public static void main(String[] args) {
+        int x ;
+        x = 500;
+        System.out.println(x);
+
+        x = x+5;
+        System.out.println(x);
+
+    }
+}
