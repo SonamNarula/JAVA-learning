dc1d6b711ad4cdcc16ad2488d504c13368362dc8
SonamNarula <sonamnarula2108@gmail.com>
2025-07-29 11:08:41 +0530

    indentation

---
+++ BasicsOfJava/indentation.java
@@ -0,0 +1,13 @@
+package BasicsOfJava;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class indentation {
+    public static void main(String[] args) {
+        {
+            System.out.println("This is inside the block.");
+        }
+
+        System.out.println("This is outside the block.");
+    }
+}
