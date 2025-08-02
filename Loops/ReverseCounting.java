4774359bf952e41c90fc02cadf3ab0229ad79b97
SonamNarula <sonamnarula2108@gmail.com>
2025-08-02 21:20:10 +0530

    reverse counting

---
+++ Loops/ReverseCounting.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class ReverseCounting {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n : ");
+        int n = sc.nextInt();
+
+        for (int i = n; i >= 1; i--) {
+            System.out.println(i);
+        }
+    }
+}
